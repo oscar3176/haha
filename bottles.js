093eaa543eb2d2cc47d09c7d2a6aa1d8c96cb0de
oscar3176 <58105187+oscar3176@users.noreply.github.com>
2022-04-12 15:12:46 +0300

    Add files via upload

---
+++ bottles.js
@@ -0,0 +1,47 @@
+object = [];
+Status = "";
+function preload() {
+    bottles = loadImage("bottles.jpg");
+}
+
+function setup() {
+    canvas = createCanvas(640, 430,);
+    canvas.position(650, 200);
+    objectDetector = ml5.objectDetector("cocossd", ModalLoaded);
+}
+
+function ModalLoaded() {
+    console.log("Modal Loaded");
+    Status = true;
+    objectDetector.detect(bottles, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    else {
+        console.log(results);
+        object = results;
+    }
+}
+
+function draw() {
+    image(bottles, 0 , 0 , 640, 430);
+    if (Status != "") {
+
+        for (i = 0; i < object.length; i++) {
+            console.log("Started Drawing");
+            fill("red");
+            stroke("red");
+            percent = floor(object[i].confidence*100);
+            text(object[i].label + " " + percent + "%", object[i].x + 15, object[i].y + 15);
+            noFill();
+            rect(object[i].x, object[i].y, object[i].width, object[i].height);
+        }
+    }
+}
+
+function back() {
+    window.location = "index.html";
+}
